9d7481988371d6df9fb2c23f5b05256efa1ef3e3
yfiqbal <42559368+yfiqbal@users.noreply.github.com>
2025-11-02 00:55:14 -0400

    Understanding of calling a function from main

---
+++ main.py
@@ -0,0 +1,8 @@
+#implement a function that splits a string by space
+def split_string_by_space(input_string):
+    return input_string.split(" ")
+"""
+to run in cmd call python
+then from main import split_string_by_space
+enter split_string_by_space("your string here")
+"""
