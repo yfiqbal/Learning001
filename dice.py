d96009d586e2b6cc6fb018401b63e4630fecad95
yfiqbal <42559368+yfiqbal@users.noreply.github.com>
2025-11-27 21:35:37 -0500

    Plot added

--- dice.py
+++ dice.py
@@ -21,7 +21,7 @@ for x in range(100) :
     dice = np.random.randint(1,7)
 
     if dice <= 2:
-        # Replace below: use max to make sure step can't go below 0
+        # Use max to make sure step can't go below 0
         step = max(0, step - 1)
     elif dice <= 5:
         step = step + 1
@@ -31,3 +31,12 @@ for x in range(100) :
     random_walk.append(step)
 
 print(random_walk)
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Plot random_walk
+plt.plot(random_walk)
+
+# Show the plot
+plt.show()
